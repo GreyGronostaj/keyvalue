644e7310fd6ed92a3bf102910ff9115f223d019d
mkasprz <mkasprz@student.agh.edu.pl>
2016-11-12 13:04:09 +0100

    Added 'sleep' in the test for Travis

--- scripts/tests/twoServersAndOneClient.sh
+++ scripts/tests/twoServersAndOneClient.sh
@@ -8,9 +8,13 @@ echo PID: $FirstServerPID.
 SecondServerPID=$(./scripts/runServer.sh 127.0.0.1:10001 127.0.0.1:10000)
 echo PID: $SecondServerPID.
 
+sleep 2
+
 ./scripts/runClient.sh 127.0.0.1:10000 put A 1
 
 ExpectedValue='1'
+
+sleep 1
 
 ReceivedValue=$(./scripts/runClient.sh 127.0.0.1:10001 get A)
 
